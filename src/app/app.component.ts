1601e97e979b8f018f802162f7e3bbc5d613b0ee
Bogdan <bodia-00wot@ukr.net>
2023-06-12 21:48:29 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Usense-task';
+  hide = true;
+  password: string = '';
+
+  checkPasswordStrength() {
+    const password = this.password;
+    let strengthSections = document.querySelectorAll('.section');
+
+    const removeColors = (index: number) => {
+      strengthSections[index].classList.remove('gray','red', 'yellow', 'green');
+    }
+
+    strengthSections.forEach(section => {
+      section.classList.remove('red', 'yellow', 'green');
+      section.classList.add('gray');
+    });
+
+    if (password.length < 8 && password.length !== 0) {
+      strengthSections.forEach((section,index) => {
+        removeColors(index);
+        section.classList.add('red');
+      });
+    } else if(password.length !== 0) {
+      if (/[a-zA-Z]/.test(password) && /[0-9]/.test(password) && /[!@#$%^&*]/.test(password)) {
+        strengthSections.forEach((section, index) => {
+          removeColors(index);
+          section.classList.add('green');
+        });
+      } else if (
+          (/[a-zA-Z]/.test(password) && /[0-9]/.test(password))
+          || (/[a-zA-Z]/.test(password) && /[!@#$%^&*]/.test(password))
+          || (/[0-9]/.test(password) && /[!@#$%^&*]/.test(password))
+          ) {
+        removeColors(0);
+        removeColors(1)
+        strengthSections[0].classList.add('yellow');
+        strengthSections[1].classList.add('yellow');
+      } else {
+        removeColors(0);
+        strengthSections[0].classList.add('red');
+      }
+    }
+
+
+
+    // if (/[a-zA-Z]/.test(password) && /[0-9]/.test(password) && /[!@#$%^&*]/.test(password)) {
+    //   strengthSections.forEach(section => {
+    //     section.classList.add('green');
+    //   });
+    // } else if(password.length < 8) {
+    //   if(password.length === 0) {
+    //     strengthSections.forEach(section => {
+    //       section.classList.remove('red', 'yellow', 'green');
+    //       section.classList.add('gray');
+    //     });
+    //   } else {
+    //     strengthSections.forEach(section => {
+    //       section.classList.add('red');
+    //     });
+    //   }
+    // } else if ((/[a-zA-Z]/.test(password) && /[0-9]/.test(password)) || (/[a-zA-Z]/.test(password) && /[!@#$%^&*]/.test(password)) || (/[0-9]/.test(password) && /[!@#$%^&*]/.test(password))) {
+    //   strengthSections[0].classList.remove('gray');
+    //   strengthSections[0].classList.add('yellow');
+    //   strengthSections[1].classList.add('yellow');
+    // } else if (password.length === 0){
+    // } else {
+    //   strengthSections[0].classList.remove('gray');
+    //   strengthSections[0].classList.add('red');
+    // }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatFormFieldModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
